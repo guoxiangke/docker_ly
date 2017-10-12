32050e32085288b8eb302ed95ca4dee7e9caded2
Your Name <you@example.com>
2017-10-12 17:40:06 +0800

    init

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define('S3KEY', 'AKIAICBSUROM4QGVKMDQ');
+define('S3SECRET', 'HI7foXeffNipcQEIqP+VUpx3CVCBLHFU6exOayD/');
+
+define('UPBUCKETNAME', 'bbn2016');
+define('UPOPNAME', 'bbn2016');
+define('UPOPPASS', 'bbn2016bbn2016');
+define('CDNLINK', UPBUCKETNAME.'.b0.upaiyun.com');
+
+define('DEBUG', TRUE);
+define('ARCHIVE', 1);
+
+date_default_timezone_set('Asia/Shanghai');
---
+++ focus.php
@@ -0,0 +1,24 @@
+<h4>后台收听</h4>
+<h5>►关注：微信回复对于编号即可↓↓↓</h5>
+<div style="display: flex;
+  justify-content: center;
+  align-items: center; "><img class="img-responsive" src="../qrcode.jpg">
+</div>
+<p>Powered by： <a href="https://www.yongbuzhixi.com">永不止息</a></p>
+<div class="bg-no">
+  <h5><a href="https://www.yongbuzhixi.com" style="color:#FFF">非诚勿点</a></h5>
+</div>
+<style type="text/css">
+  .bg-no h5{
+    display: none;
+    padding-left: 10px;
+  }
+</style>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-36975988-3', 'auto');
+  ga('send', 'pageview');
+</script>
