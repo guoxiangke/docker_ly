18198a671487302c0a0155c34d406fce56c1e48b
Your Name <you@example.com>
2017-10-20 17:01:35 +0800

    rename

--- auto.sh
+++ auto.sh
@@ -3,6 +3,6 @@ pwd
 git pull
 git push
 ssh root@bce.yongbuzhixi.com<< EOF
-cd ~/docker/docker_ly_player && git pull 
+cd ~/docker/docker_ly && git pull 
 docker-compose -p ly -f ./docker-compose.yml up -d  --build --remove-orphans --force-recreate
 EOF
